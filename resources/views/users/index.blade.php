c9f6a7e329fd1d630b8ef4764e2fc5a3398572a9
mdanu444 <47779080+mdanu444@users.noreply.github.com>
2022-02-17 17:22:24 +0600

    somthing updated

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -8,7 +8,7 @@
 <!-- DataTales Example -->
 <div class="card shadow mb-4">
     <div class="card-header py-3">
-        <h6 class="m-0 font-weight-bold text-primary">User Groups List</h6>
+        <h6 class="m-0 font-weight-bold text-primary">Users List</h6>
     </div>
     <div class="card-body">
         <div class="table-responsive">
